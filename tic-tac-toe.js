8e4e7fc8be702d74bc2c3df1f53cfaa4d32b25d0
Cargill Seiveright <microcargill@hotmail.com>
2019-10-15 16:17:34 -0500

    Winner message is displaying

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,20 +1,17 @@
 let layout = document.querySelector("link"); //Creates a variable to reference the link tag from the HTML
 let squares; 
 let alternate = false;
+let messageBox;
 
 layout.addEventListener("load", function(){ //Waits until the CSS layout is loaded
     squares = document.querySelector("#board");//Selects the div element with all the div section for the 3x3 squares
     squares = squares.querySelectorAll("div"); //Selects all sub div elements
+    messageBox = document.querySelector("#status");//Selects the message box
+    
     squares.forEach(element => {
         element.setAttribute("class","square");//Adds class with value square to each div
     });
     
-    let index = 0;
-    squares.forEach(element => {
-        element.setAttribute("id", index);
-        index++;
-    });
-
     squares.forEach(element => {
         element.addEventListener("click", function(){
             if (!(element.getAttribute("class").includes("X") || element.getAttribute("class").includes("O"))) {//Checks whether the square has already been assigned X or O, if not it will be assigned
@@ -28,7 +25,7 @@ layout.addEventListener("load", function(){ //Waits until the CSS layout is load
                     alternate = false;
                 }
             }
-            winCheck();
+            winCheck(squares, messageBox);
         });
 
         element.addEventListener("mouseover", function(){
@@ -47,6 +44,27 @@ layout.addEventListener("load", function(){ //Waits until the CSS layout is load
 
 });//end of waiting on the CSS stylesheet to load
 
-const winCheck = function(){
-    console.log("hi")
+const winCheck = function(tile, box){
+    if (tile[0].innerHTML === tile[1].innerHTML && tile[1].innerHTML === tile[2].innerHTML && tile[0].innerHTML === tile[2].innerHTML && tile[2].innerHTML !== "") { // 1 of 8 win possibilities
+        displayMessage(box, tile[0].innerHTML);
+    } else if (tile[3].innerHTML === tile[4].innerHTML && tile[4].innerHTML === tile[5].innerHTML && tile[3].innerHTML === tile[5].innerHTML && tile[5].innerHTML !== "") { // 2 of 8 win possibilities
+        displayMessage(box, tile[3].innerHTML);;
+    } else if (tile[6].innerHTML === tile[7].innerHTML && tile[7].innerHTML === tile[8].innerHTML && tile[6].innerHTML === tile[8].innerHTML && tile[8].innerHTML !== "") { // 3 of 8 win possibilities
+        displayMessage(box, tile[6].innerHTML);;
+    } else if (tile[0].innerHTML === tile[3].innerHTML && tile[3].innerHTML === tile[6].innerHTML && tile[0].innerHTML === tile[6].innerHTML && tile[6].innerHTML !== "") { // 4 of 8 win possibilities
+        displayMessage(box, tile[0].innerHTML);;
+    } else if (tile[1].innerHTML === tile[4].innerHTML && tile[4].innerHTML === tile[7].innerHTML && tile[1].innerHTML === tile[7].innerHTML && tile[7].innerHTML !== "") { // 5 of 8 win possibilities
+        displayMessage(box, tile[1].innerHTML);;
+    } else if (tile[2].innerHTML === tile[5].innerHTML && tile[5].innerHTML === tile[8].innerHTML && tile[2].innerHTML === tile[8].innerHTML && tile[8].innerHTML !== "") { // 6 of 8 win possibilities
+        displayMessage(box, tile[2].innerHTML);;
+    } else if (tile[0].innerHTML === tile[4].innerHTML && tile[4].innerHTML === tile[8].innerHTML && tile[0].innerHTML === tile[8].innerHTML && tile[8].innerHTML !== "") { // 7 of 8 win possibilities
+        displayMessage(box, tile[0].innerHTML);;
+    } else if (tile[2].innerHTML === tile[4].innerHTML && tile[4].innerHTML === tile[6].innerHTML && tile[2].innerHTML === tile[6].innerHTML && tile[6].innerHTML !== "") { // 8 of 8 win possibilities
+        displayMessage(box, tile[2].innerHTML);;
+    }
+};//end of winCheck function that checks if there are matching lines of X or O
+
+const displayMessage = function(mBox, xO){
+    mBox.setAttribute("class", "you-won");
+    mBox.innerHTML = `Congratulations! ${xO} is the Winner!`;
 };
